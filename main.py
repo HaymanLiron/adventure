b84d79846a3a6e965993415e111c92f961d942ca
Liron <liron.hayman@gmail.com>
2016-08-11 16:34:32 +0300

    port change

--- main.py
+++ main.py
@@ -102,10 +102,8 @@ def images(filename):
 
 
 def main():
-    if os.environ.get('APP_LOCATION') == 'heroku':
-        run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    else:
-        run(host='localhost', port=8080, debug=True)
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
 
 if __name__ == '__main__':
